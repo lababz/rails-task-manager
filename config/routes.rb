72033ca1b1d61284ba9667cccf9d52a478fa6e58
Hadrien G <hgrach@gmail.com>
2023-08-22 16:15:00 +0200

    App done V1 - No bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "tasks#index"
   get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new_task
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
   get "tasks/:id", to: "tasks#show", as: :task
 end
